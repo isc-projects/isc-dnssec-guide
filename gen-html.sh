350b3d3ccf248653e6b032c76fbe26fc6a56414f
Josh Kuo <josh@deepdivenetworking.com>
2014-09-30 20:50:42 +0800

    Initial commit

---
+++ gen-html.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script is a shortcut to quickly generate a single HTML
+# file from the DocBook XML source in this directory
+xsltproc --novalid --xinclude --nonet \
+         -o doc/dnssec-guide.html --stringparam section.autolabel 1 \
+         --stringparam section.label.includes.component.label 1 \
+         --stringparam html.stylesheet dnssec-guide.css http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl \
+         src/dnssec-guide.xml
