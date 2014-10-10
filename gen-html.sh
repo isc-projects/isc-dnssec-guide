e4ec672ae7c51a1acb2f91b5b5ef726bd778c685
Jeremy C. Reed <jreed@isc.org>
2014-10-10 13:01:40 -0500

    copy the css to the generated docs directory
    and fix the path to the css style sheet
    this is because the ../src/ probably doesn't exist on target system
    that the html file is copied too

--- gen-html.sh
+++ gen-html.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # This script is a shortcut to quickly generate a single HTML
 # file from the DocBook XML source in this directory
+
+mkdir -p doc
+cp src/dnssec-guide.css doc/
+
 xsltproc --novalid --xinclude --nonet \
          -o doc/dnssec-guide.html --stringparam section.autolabel 1 \
          --stringparam section.label.includes.component.label 1 \
-         --stringparam html.stylesheet ../src/dnssec-guide.css http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl \
+         --stringparam html.stylesheet dnssec-guide.css http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl \
          src/dnssec-guide.xml
